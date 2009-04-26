e6e65423fa5ef08a35a31ad9c8864beb81418f19
Hans Dieter Pearcey <hdp@weftsoar.net>
2009-04-25 23:14:43 -0400

    setup script

---
+++ script/bugz_setup.pl
@@ -0,0 +1,29 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+use Cwd ();
+use File::Spec;
+use File::Basename;
+
+die "usage: $0 /path/to/bugzilla\n"
+  unless @ARGV;
+
+my $bugz_path = Cwd::abs_path(shift);
+
+unlink(
+  glob('root/cgi-bin/*'),
+  qw(root/images root/js root/skins)
+);
+
+for my $cgi (glob("$bugz_path/*.cgi")) {
+  my $dest = "root/cgi-bin/" . basename($cgi);
+  symlink($cgi, $dest) or die "Can't symlink $dest -> $cgi: $!";
+  print "$dest -> $cgi\n";
+}
+
+for my $dir (qw(images js skins)) {
+  symlink("$bugz_path/$dir", "root/$dir")
+    or die "Can't symlink root/$dir -> $bugz_path/$dir: $!";
+  print "root/$dir -> $bugz_path/$dir\n";
+}
